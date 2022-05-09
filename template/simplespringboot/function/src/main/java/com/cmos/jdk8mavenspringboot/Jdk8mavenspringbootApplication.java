b6751c5b612ac2d1a38a03bad8138edb0931c5c0
15938742355 <liuqinxiao@cmos.chinamobile.com>
2022-05-09 21:03:40 +0800

    自制SpringBoot模板

---
+++ template/simplespringboot/function/src/main/java/com/cmos/jdk8mavenspringboot/Jdk8mavenspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.cmos.jdk8mavenspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jdk8mavenspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jdk8mavenspringbootApplication.class, args);
+    }
+
+}
---
+++ template/simplespringboot/function/src/main/java/com/cmos/jdk8mavenspringboot/controller/Hello.java
@@ -0,0 +1,15 @@
+package com.cmos.jdk8mavenspringboot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
+
+@RestController
+public class Hello {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String hello() {
+        return "Hello world, " + new Date();
+    }
+}
